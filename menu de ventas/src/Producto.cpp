c643bf3f67cb2bb3b6343d98ec911d2db03700d8
Steven-umg <svasquezc12@miumg.edu.gt>
2025-04-03 18:58:36 -0600

    Producto.cpp

--- menu de ventas/src/Producto.cpp
+++ menu de ventas/src/Producto.cpp
@@ -1,3 +1,11 @@
 #include "Producto.h"
 
+// Constructor de la clase Producto.
+// Este constructor inicializa un objeto de la clase Producto utilizando los parámetros proporcionados
+//
+// Parámetros:
+//   n (std::string): El nombre del producto.
+//   p (double): El precio del producto.
+//
+// Este constructor asigna estos valores a los atributos correspondientes del objeto actual.
 Producto::Producto(std::string n, double p) : nombre(n), precio(p) {}
