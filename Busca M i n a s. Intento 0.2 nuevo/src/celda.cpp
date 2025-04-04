69e958d1b51804a52c3cf5fe04c266bca3947e32
marlondeleon17 <100364159+marlondeleon17@users.noreply.github.com>
2025-04-03 18:01:05 -0600

    Update celda.cpp
    cambios de documentación

--- Busca M i n a s. Intento 0.2 nuevo/src/celda.cpp
+++ Busca M i n a s. Intento 0.2 nuevo/src/celda.cpp
@@ -1,7 +1,6 @@
-#include "Celda.h"
+#include "Celda.h" // Constructor de la clase Celda
 
-// Constructor de la clase Celda
-// Inicializa una nueva celda con los siguientes valores:
-// - mina: false (indica que la celda no contiene una mina)
-// - descubierta: false (indica que la celda no ha sido descubierta)
+// Inicializa una nueva celda con los siguientes valores predeterminados:
+// - mina: false (indica que la celda no contiene una mina inicialmente)
+// - descubierta: false (indica que la celda no ha sido revelada aún)
 Celda::Celda() : mina(false), descubierta(false) {}
